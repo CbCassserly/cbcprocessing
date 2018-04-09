7442e6d544dbb688bd1546db6a144234fb684dad
Surya Oktafendri <f2face@f2face.com>
2018-04-09 19:27:45 +0700

    Version 2.0

--- main.js
+++ main.js
@@ -1,94 +1,95 @@
+"use strict";
+
 console.log("bot is starting");
 
-//twit package
 var Twit = require('twit');
-
 var config = require('./config');
-
-var T = new Twit(config);
-
-//image downloader package
-const download = require('image-downloader');
 
 //image processing package
 const Jimp = require("jimp");
 
+var T = new Twit(config);
+
+var addTextToImage = function (text, output_image_file, success_callback) {
+    console.log('Getting a random image from Unsplash...');
+    Jimp.read('https://unsplash.it/400/600/?random').then(function (image) {
+        // Text font
+        var text_font = Jimp.FONT_SANS_16_WHITE;
+
+        // Left margin
+        var text_x_offset = 20;
+
+        // Top margin
+        var text_y_offset = 20;
+
+        // Text max width
+        var text_max_width = 400;
+
+        // Process the image
+        image.brightness( -0.5 );
+        //image.fade(0.5);
+
+        // Add text to the image
+        console.log('Adding text to the image');
+        Jimp.loadFont(text_font).then(function (font) {
+            image.print(font, text_x_offset, text_y_offset, text, text_max_width)
+                .write(output_image_file, success_callback);
+        });
+    }).catch(function (err) {
+        console.log(err);
+    });
+}
+
+var postTweetImage = function (image_file, alt_text, tweet) {
+    var fs = require('fs');
+    var b64content = fs.readFileSync(image_file, { encoding: 'base64' });
+
+    // Upload the image first
+    console.log('Uploading the image...');
+    T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+        if (!err) {
+            var media_id = data.media_id_string;
+            var meta_params = {
+                media_id: media_id,
+                alt_text: {
+                    text: alt_text
+                }
+            };
+
+            // Assign alt text to the uploaded media, for use by screen readers and
+            // other text-based presentations and interpreters
+            T.post('media/metadata/create', meta_params, function (err, data, response) {
+                if (!err) {
+                    var params = {
+                        status: tweet || '',
+                        media_ids: [ media_id ]
+                    };
+
+                    // Post the tweet
+                    console.log('Sending tweet...');
+                    T.post('statuses/update', params, function (err, data, response) {
+                        if (!err) {
+                            console.log('Tweet sent!');
+                        } else {
+                            console.log(err);
+                        }
+                    });
+                } else {
+                    console.log(err);
+                }
+            });
+        } else {
+            console.log(err);
+        }
+    });
+}
 
 var stream = T.stream('user');
-
-		function down() {
-
-			var stream = T.stream('user');
-
-			 stream.on('direct_message', function (data) {
-			 
-				var req = data.direct_message.text;
-
-					//download photo
-					options = {
-					  url: 'https://unsplash.it/400/600/?random',
-					  dest: 'download/photo.jpg'        // Save to /path/to/dest/photo.jpg 
-					}
-
-					download.image(options)
-					  .then(({ filename, image }) => {
-					    console.log('File saved to', filename)
-					  }).catch((err) => {
-					    throw err
-					  })
-
-
-					  	// edit photo
-						Jimp.read("download/photo.jpg").then(function (image) {
-
-							image.brightness( -0.5 );
-						   
-					    	Jimp.loadFont(Jimp.FONT_SANS_32_WHITE).then(function (font) {
-							    image.print(font, 20, 20, req, 2)
-							     .write("upload/photo-edited.png"); // save;
-							     console.log("file edited");
-							});
-
-
-						}).catch(function (err) {
-						    console.log(err);
-						});
-
-
-						//tweet photo
-						function twot() {
-							
-						var fs = require('fs');
-						var b64content = fs.readFileSync('upload/photo-edited.png', { encoding: 'base64' });
-
-						T.post('media/upload', { media_data: b64content }, function (err, data, response) {
-						  // now we can assign alt text to the media, for use by screen readers and 
-						  // other text-based presentations and interpreters 
-						  var mediaIdStr = data.media_id_string
-						  var altText = "Small flowers in a planter on a sunny balcony, blossoming."
-						  var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
-						 
-						  T.post('media/metadata/create', meta_params, function (err, data, response) {
-						    if (!err) {
-						      // now we can reference the media and post a tweet (media will attach to the tweet) 
-						      var params = { status: '', media_ids: [mediaIdStr] }
-						 
-						      T.post('statuses/update', params, function (err, data, response) {
-						        console.log(data)
-						      })
-						    }
-						  })
-						})
-
-
-
-						}
-
-						setTimeout(twot, 3000);
-		
-
-			});
-		}
-
-
-down();
+stream.on('direct_message', function (data) {
+    console.log('Reading a DM...');
+    var dm_text = data.direct_message.text;
+    var tmp_img = 'tmp/image_tmp.jpg';
+    addTextToImage(dm_text, tmp_img, function () {
+        postTweetImage(tmp_img, dm_text);
+    });
+});
