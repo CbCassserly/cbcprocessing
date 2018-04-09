c48d99b33d7d6611c69a69ebd92bee67bcfd87fb
Surya Oktafendri <f2face@f2face.com>
2018-04-09 21:58:41 +0700

    Make sure JSHint happy

--- main.js
+++ main.js
@@ -1,95 +1,96 @@
-"use strict";
+(function(){
+    "use strict";
 
-console.log("bot is starting");
+    console.log("Bot is starting");
 
-var Twit = require('twit');
-var config = require('./config');
+    // Load required modules/files
+    var Twit = require('twit');
+    var Jimp = require("jimp");
+    var config = require('./config');
 
-//image processing package
-const Jimp = require("jimp");
+    // Initialize new Twit instance
+    var T = new Twit(config);
 
-var T = new Twit(config);
+    var addTextToImage = function (text, output_image_file, success_callback) {
+        console.log('Getting a random image from Unsplash...');
+        Jimp.read('https://unsplash.it/400/600/?random').then(function (image) {
+            // Text font
+            var text_font = Jimp.FONT_SANS_16_WHITE;
 
-var addTextToImage = function (text, output_image_file, success_callback) {
-    console.log('Getting a random image from Unsplash...');
-    Jimp.read('https://unsplash.it/400/600/?random').then(function (image) {
-        // Text font
-        var text_font = Jimp.FONT_SANS_16_WHITE;
+            // Left margin
+            var text_x_offset = 20;
 
-        // Left margin
-        var text_x_offset = 20;
+            // Top margin
+            var text_y_offset = 20;
 
-        // Top margin
-        var text_y_offset = 20;
+            // Text max width
+            var text_max_width = 400;
 
-        // Text max width
-        var text_max_width = 400;
+            // Process the image
+            image.brightness( -0.5 );
 
-        // Process the image
-        image.brightness( -0.5 );
-        //image.fade(0.5);
-
-        // Add text to the image
-        console.log('Adding text to the image');
-        Jimp.loadFont(text_font).then(function (font) {
-            image.print(font, text_x_offset, text_y_offset, text, text_max_width)
-                .write(output_image_file, success_callback);
-        });
-    }).catch(function (err) {
-        console.log(err);
-    });
-}
-
-var postTweetImage = function (image_file, alt_text, tweet) {
-    var fs = require('fs');
-    var b64content = fs.readFileSync(image_file, { encoding: 'base64' });
-
-    // Upload the image first
-    console.log('Uploading the image...');
-    T.post('media/upload', { media_data: b64content }, function (err, data, response) {
-        if (!err) {
-            var media_id = data.media_id_string;
-            var meta_params = {
-                media_id: media_id,
-                alt_text: {
-                    text: alt_text
-                }
-            };
-
-            // Assign alt text to the uploaded media, for use by screen readers and
-            // other text-based presentations and interpreters
-            T.post('media/metadata/create', meta_params, function (err, data, response) {
-                if (!err) {
-                    var params = {
-                        status: tweet || '',
-                        media_ids: [ media_id ]
-                    };
-
-                    // Post the tweet
-                    console.log('Sending tweet...');
-                    T.post('statuses/update', params, function (err, data, response) {
-                        if (!err) {
-                            console.log('Tweet sent!');
-                        } else {
-                            console.log(err);
-                        }
-                    });
-                } else {
-                    console.log(err);
-                }
+            // Add text to the image
+            console.log('Adding text to the image');
+            Jimp.loadFont(text_font).then(function (font) {
+                image.print(font, text_x_offset, text_y_offset, text, text_max_width)
+                    .write(output_image_file, success_callback);
             });
-        } else {
+        }).catch(function (err) {
             console.log(err);
-        }
-    });
-}
+        });
+    };
 
-var stream = T.stream('user');
-stream.on('direct_message', function (data) {
-    console.log('Reading a DM...');
-    var dm_text = data.direct_message.text;
-    var tmp_img = 'tmp/image_tmp.jpg';
-    addTextToImage(dm_text, tmp_img, function () {
-        postTweetImage(tmp_img, dm_text);
+    var postTweetImage = function (image_file, alt_text, tweet) {
+        var fs = require('fs');
+        var b64content = fs.readFileSync(image_file, { encoding: 'base64' });
+
+        // Upload the image first
+        console.log('Uploading the image...');
+        T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+            if (!err) {
+                var media_id = data.media_id_string;
+                var meta_params = {
+                    media_id: media_id,
+                    alt_text: {
+                        text: alt_text
+                    }
+                };
+
+                // Assign alt text to the uploaded media, for use by screen readers and
+                // other text-based presentations and interpreters
+                T.post('media/metadata/create', meta_params, function (err, data, response) {
+                    if (!err) {
+                        var params = {
+                            status: tweet || '',
+                            media_ids: [ media_id ]
+                        };
+
+                        // Post the tweet
+                        console.log('Sending tweet...');
+                        T.post('statuses/update', params, function (err, data, response) {
+                            if (!err) {
+                                console.log('Tweet sent!');
+                            } else {
+                                console.log(err);
+                            }
+                        });
+                    } else {
+                        console.log(err);
+                    }
+                });
+            } else {
+                console.log(err);
+            }
+        });
+    };
+
+    var stream = T.stream('user');
+    stream.on('direct_message', function (data) {
+        console.log('Reading a DM...');
+        var dm_text = data.direct_message.text;
+        var tmp_img = 'tmp/image_tmp.jpg';
+        addTextToImage(dm_text, tmp_img, function () {
+            postTweetImage(tmp_img, dm_text);
+        });
     });
-});
+})();
--- post-install-script.js
+++ post-install-script.js
@@ -3,6 +3,10 @@ var fs = require('fs');
 if (!fs.existsSync('./config.js')) {
     var copyFile = require('quickly-copy-file');
     copyFile('./config-example.js', 'config.js')
-        .then(() => console.log('config-example.js has been successfully copied to config.js'))
-        .catch(error => console.error('An error occured. You may need to manually copy and rename config-example.js to config.js'));
+        .then(function () {
+            console.log('config-example.js has been successfully copied to config.js');
+        })
+        .catch(function (error) {
+            console.error('An error occured. You may need to manually copy and rename config-example.js to config.js');
+        });
 }
