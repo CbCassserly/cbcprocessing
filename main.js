c81fd540179eaeea6b2ee77a3d36e8531f06bed2
Surya Oktafendri <f2face@f2face.com>
2018-04-10 09:54:03 +0700

    Auto set smaller font size for message with more than 15 words

--- main.js
+++ main.js
@@ -15,7 +15,7 @@
         console.log('Getting a random image from Unsplash...');
         Jimp.read('https://unsplash.it/400/600/?random').then(function (image) {
             // Text font
-            var text_font = Jimp.FONT_SANS_16_WHITE;
+            var text_font = Jimp.FONT_SANS_32_WHITE;
 
             // Left margin
             var text_x_offset = 20;
@@ -23,14 +23,20 @@
             // Top margin
             var text_y_offset = 20;
 
-            // Text max width
-            var text_max_width = 400;
+            // Text max width.
+            // Value lower than the image width may make the text get wrapped.
+            var text_max_width = 2;
+
+            // If text is wrapped and there are more than 15 words, change to a smaller font.
+            if (text_max_width <= 2 && text.split(' ').length > 15) {
+                text_font = Jimp.FONT_SANS_16_WHITE;
+            }
 
             // Process the image
-            image.brightness( -0.5 );
+            image.brightness(-0.5);
 
             // Add text to the image
-            console.log('Adding text to the image');
+            console.log('Adding text to the image...');
             Jimp.loadFont(text_font).then(function (font) {
                 image.print(font, text_x_offset, text_y_offset, text, text_max_width)
                     .write(output_image_file, success_callback);
@@ -86,7 +92,7 @@
 
     var stream = T.stream('user');
     stream.on('direct_message', function (data) {
-        console.log('Reading a DM...');
+        console.log('Reading new direct message...');
         var dm_text = data.direct_message.text;
         var tmp_img = 'tmp/image_tmp.jpg';
         addTextToImage(dm_text, tmp_img, function () {
